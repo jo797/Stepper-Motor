4727d7361b4dd2f6ac5d28053e7380a1d23af646
AnOatmealCookie <cookas@mcmaster.ca>
2017-03-27 20:48:35 -0400

    Update decoder.v

--- Desktop/3TB4/lab5_starter_restored/decoder.v
+++ Desktop/3TB4/lab5_starter_restored/decoder.v
@@ -4,15 +4,16 @@ module decoder (input [7:2] instruction,
 
 assign br = (instruction[7:5] == 3'b100);
 assign clr = (instruction[7:2] == 6'b011000);
-//assign brz = (...);
-//assign addi = (...);
-//assign subi = (...);
-//assign sr0 = (...);
-//assign srh0 = (...);
-//assign mov = (...);
-//assign mova = (...);
-//assign movr = (...);
-//assign movrhs = (...);
-//assign pause = (...);
+assign brz = (instruction[7:5]==3'b101);
+assign addi = (instruction[7:5]==3'b000);
+assign subi = (instruction[7:5]==3'b001);
+assign sr0 = (instruction[7:4]==4'b0100);
+assign srh0 = (instruction[7:4]==4'b0101);
+assign mov = (instruction[7:4]==4'b0111);
+assign mova = (instruction[7:2]==6'b110000);
+assign movr = (instruction[7:2]==6'b110001);
+assign movrhs = (instruction[7:2]==6'b110010);
+assign pause = (instruction[7:0]==8'b11111111);
+
 
 endmodule
