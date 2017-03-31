90ad3f7b34da2f01cdbccea3786722dcb8fc6d52
AnOatmealCookie <cookas@mcmaster.ca>
2017-03-31 22:26:10 +0100

    Update pc.v

--- Desktop/3TB4/lab5_starter_restored/pc.v
+++ Desktop/3TB4/lab5_starter_restored/pc.v
@@ -1,20 +1,19 @@
 module pc (input clk, reset_n, branch, increment, input [7:0] newpc,
 			output reg [7:0] pc);
+			
 parameter RESET_LOCATION = 8'h00;
 
-always@(posedge)
-	begin 
-		if(reset_n)
-			begin
-				pc = RESET_LOCATION;
-			end
-		if(branch)
-			begin
-				pc = newpc;
-			end 
-		if(increment)
-			begin
-				pc = pc + 8'h01;
-			end
+always @( posedge clk ) begin 
+	if(!reset_n) begin
+		pc <= RESET_LOCATION;
 	end
+	if(branch) begin
+		pc<= newpc;
+	end 
+	if(increment) begin
+		pc <= pc + 8'h01;
+	end
+end
+
 endmodule
+
