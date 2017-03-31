e189e1d871757cd851d5a630411e733c9838d8f8
AnOatmealCookie <cookas@mcmaster.ca>
2017-03-31 22:27:00 +0100

    done

--- Desktop/3TB4/lab5_starter_restored/op1_mux.v
+++ Desktop/3TB4/lab5_starter_restored/op1_mux.v
@@ -1,6 +1,13 @@
 module op1_mux (input [1:0] select, input [7:0] pc, register, register0, position,
-				output reg [7:0] result);
+			output reg [7:0] result);
 
-
-				
+always@(*) begin
+	case(select)
+		2'b00: result <= pc;
+		2'b01: result <= register;
+		2'b10: result <= register0;
+		2'b11: result <= position;
+	endcase
+end
 endmodule
+
