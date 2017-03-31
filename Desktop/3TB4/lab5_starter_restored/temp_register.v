a51f5961e86206d731be9f6cc1d8751d0b85104b
Joletta Cheung <jo797@users.noreply.github.com>
2017-03-31 15:40:45 -0400

    done?

--- Desktop/3TB4/lab5_starter_restored/temp_register.v
+++ Desktop/3TB4/lab5_starter_restored/temp_register.v
@@ -1,6 +1,31 @@
 module temp_register (input clk, reset_n, load, increment, decrement, input [7:0] data,
 					output negative, positive, zero);
 
-
+	
+reg [31:0] counter=0;
+reg [7:0] temp ;
+//assume active low
+always @(*)
+begin
+	if (load==0)
+	begin
+		//number of steps/HS that need to happen
+		temp<= data;
+	end
+	if (increment==0)
+	begin
+		counter<= counter++;
+	end
+	if (decrement==0)
+	begin
+		counter<= counter--;
+	end
+	if(data <0)
+		negative = 1;
+	else if (data == 0)
+		zero=1;
+	else
+		positive=1;
+end
 
 endmodule
