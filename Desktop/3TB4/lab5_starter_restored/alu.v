738d7d0c2218c81459064ec9e0d8af5f9e909e21
Joletta Cheung <jo797@users.noreply.github.com>
2017-03-31 15:14:45 -0400

    done

--- Desktop/3TB4/lab5_starter_restored/alu.v
+++ Desktop/3TB4/lab5_starter_restored/alu.v
@@ -1,5 +1,22 @@
 module alu (input add_sub, set_low, set_high, input [7:0] operanda , operandb, output reg [7:0] result);
-
-
+//assume active low
+if (set_low==0)
+	begin
+    result <= {operanda[7:4], operandb[3:0]};
+	end
+else if (set_high==0)
+	begin
+    result <= {operandb[3:0], operanda[3:0]};
+	end
+  else 
+    if(add_sub==0) // addition
+    begin
+      result <= operanda+operandb;
+    end
+    else
+      begin
+        result <= operanda-operandb;
+      end
+  end
 
 endmodule
